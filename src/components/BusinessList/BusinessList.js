0f491be6a98b0be723785de278d45ed902bbf33b
Ben Whoriskey <b.whoriskey@gmail.com>
2018-08-02 19:24:48 +0100

    moved business info to App component, now passes to child components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,16 +3,14 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
-
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />
+          })
+        }
       </div>
     );
   }
